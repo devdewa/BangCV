04fa28966dabaed6b6ef48cf248cc7dca95d613b
unknown <dewagaming123.sp@gmail.com>
2022-07-01 14:28:32 +0700

    Update Harga to 20 Ribu

--- isi.js
+++ isi.js
@@ -5,49 +5,49 @@ const vm = new Vue({
         companies: [
             {
                 judul: "CV1", modal: "modal-1",
-                harga: "Rp10.000",
+                harga: "Rp20.000",
                 image: "img/cv/CV1.jpg",
                 link: "https://api.whatsapp.com/send?phone=6285280146477&text=Halo%20Saya%20ingin%20membeli%20CV1."
             },
             {
                 judul: "CV2", modal: "modal-2",
-                harga: "Rp10.000",
+                harga: "Rp20.000",
                 image: "img/cv/CV2.jpg",
                 link: "https://api.whatsapp.com/send?phone=6285280146477&text=Halo%20Saya%20ingin%20membeli%20CV2."
             },
             {
                 judul: "CV3", modal: "modal-3",
-                harga: "Rp10.000",
+                harga: "Rp20.000",
                 image: "img/cv/CV3.jpg",
                 link: "https://api.whatsapp.com/send?phone=6285280146477&text=Halo%20Saya%20ingin%20membeli%20CV3."
             },
             {
                 judul: "CV4", modal: "modal-4",
-                harga: "Rp10.000",
+                harga: "Rp20.000",
                 image: "img/cv/CV4.jpg",
                 link: "https://api.whatsapp.com/send?phone=6285280146477&text=Halo%20Saya%20ingin%20membeli%20CV4."
             },
             {
                 judul: "CV5", modal: "modal-5",
-                harga: "Rp10.000",
+                harga: "Rp20.000",
                 image: "img/cv/CV5.jpg",
                 link: "https://api.whatsapp.com/send?phone=6285280146477&text=Halo%20Saya%20ingin%20membeli%20CV5."
             },
             {
                 judul: "CV6", modal: "modal-6",
-                harga: "Rp10.000",
+                harga: "Rp20.000",
                 image: "img/cv/CV6.jpg",
                 link: "https://api.whatsapp.com/send?phone=6285280146477&text=Halo%20Saya%20ingin%20membeli%20CV6."
             },
             {
                 judul: "CV7", modal: "modal-7",
-                harga: "Rp10.000",
+                harga: "Rp20.000",
                 image: "img/cv/CV7.jpg",
                 link: "https://api.whatsapp.com/send?phone=6285280146477&text=Halo%20Saya%20ingin%20membeli%20CV7."
             },
             {
                 judul: "CV8", modal: "modal-8",
-                harga: "Rp10.000",
+                harga: "Rp20.000",
                 image: "img/cv/CV8.jpg",
                 link: "https://api.whatsapp.com/send?phone=6285280146477&text=Halo%20Saya%20ingin%20membeli%20CV8."
             },
